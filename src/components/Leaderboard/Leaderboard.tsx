14c38f843e1b70aee2e989b59e2b623fc26f66dd
alicanerdurmaz <alicanerdurmaz@gmail.com>
2021-03-14 19:35:08 +0300

    leaderboard ui

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,9 +1,60 @@
-import Modal from 'components/Modal/Modal'
+import TrophyIcon from 'components/Icons/TrophyIcon'
 
+import styles from './Leaderboard.module.scss'
+
+const data = [
+  {
+    username: 'user 1',
+    score: 100,
+  },
+  {
+    username: 'user 1',
+    score: 100,
+  },
+  {
+    username: 'user 1',
+    score: 100,
+  },
+  {
+    username: 'user 1',
+    score: 100,
+  },
+  {
+    username: 'user 1',
+    score: 100,
+  },
+  {
+    username: 'user 1',
+    score: 100,
+  },
+  {
+    username: 'user 1',
+    score: 100,
+  },
+]
 const Leaderboard = () => {
   return (
-    <div>
-      <h1>LEADERBOARD</h1>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <TrophyIcon />
+        <h1>Leaderboard</h1>
+      </div>
+
+      <ul className={styles.list}>
+        {data.map((user, index) => {
+          return (
+            <li className={styles.listItem} key={user.username}>
+              <small>{index + 1}</small>
+              <p className={styles.username}>{user.username}</p>
+              <h2 className={styles.score}>{user.score}</h2>
+            </li>
+          )
+        })}
+      </ul>
+      <footer className={styles.footer}>
+        <p className={styles.username}>Your Score</p>
+        <h2 className={styles.score}>99</h2>
+      </footer>
     </div>
   )
 }
