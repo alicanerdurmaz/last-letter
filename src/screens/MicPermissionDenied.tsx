198f8df65de50c5e1ac56dd24364a1d7cb1f6030
alicanerdurmaz <alicanerdurmaz@gmail.com>
2021-03-16 00:23:33 +0300

    mic permission page ui

--- src/screens/MicPermissionDenied.tsx
+++ src/screens/MicPermissionDenied.tsx
@@ -1,5 +1,32 @@
+import MicrophoneIcon from 'components/Icons/MicrophoneIcon'
+import { useInternalizationCtx } from 'context/Internalization/InternalizationContext'
+
+import styles from './MicPermissionDenied.module.scss'
+
 const MicPermissionDenied = () => {
-  return <div>mikrofon engelli</div>
+  const { t } = useInternalizationCtx()
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>
+        <MicrophoneIcon enabled={false} />
+      </div>
+
+      <h1 className={styles.title}>{t('mickBlocked')} 😞</h1>
+
+      <div className={styles.desc}>
+        <p>{t('micBlockedDesc')}</p>
+        <p>{t('howToGiveMicPermission')}</p>
+      </div>
+
+      <a
+        className={styles.link}
+        target="_blank"
+        href="https://support.google.com/chrome/answer/2693767?co=GENIE.Platform%3DDesktop&hl=tr#zippy="
+      >
+        {t('howToGiveMicPermissionLink')}
+      </a>
+    </div>
+  )
 }
 
 export default MicPermissionDenied
