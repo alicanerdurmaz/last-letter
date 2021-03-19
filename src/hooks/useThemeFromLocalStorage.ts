4ed2b04087f4676f45cb6318d7d450fab17ef1a6
alicanerdurmaz <alicanerdurmaz@gmail.com>
2021-03-19 13:00:23 +0300

    feat : theme change animation

--- src/hooks/useThemeFromLocalStorage.ts
+++ src/hooks/useThemeFromLocalStorage.ts
@@ -2,7 +2,7 @@ export const useThemeFromLocalStorage = () => {
   const root = document.documentElement
   const theme = localStorage.getItem('theme')
 
-  if (theme && theme !== root.getAttribute('data-theme')) {
+  if (theme) {
     root.setAttribute('data-theme', theme)
   } else {
     root.setAttribute('data-theme', 'dark')
