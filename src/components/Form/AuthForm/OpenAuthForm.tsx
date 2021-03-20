404574b07d66d88f536e6ffb1e7ac80b0cd6307a
alicanerdurmaz <alicanerdurmaz@gmail.com>
2021-03-20 03:07:40 +0300

    feat : signin-signup button css change

--- src/components/Form/AuthForm/OpenAuthForm.tsx
+++ src/components/Form/AuthForm/OpenAuthForm.tsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 
+import cx from 'classnames'
+
 import Modal from 'components/Modal/Modal'
+import { useAuthContext } from 'context/Auth/AuthContext'
 import { useInternalizationCtx } from 'context/Internalization/InternalizationContext'
 import { Routes, useRouterContext } from 'context/Router/RouterContext'
+import { auth } from 'hooks/useFirebase'
 
 import AuthForm, { FormType } from './AuthForm'
-import styles from './AuthForm.module.scss'
+import styles from './OpenAuthForm.module.scss'
 
 const OpenAuthForm = () => {
+  const { currentUser } = useAuthContext()
   const { getActiveRoute, changeRoute } = useRouterContext()
   const { t } = useInternalizationCtx()
   const [isAuthFormOpen, setIsAuthFormOpen] = useState<FormType>(FormType.closed)
 
-  const onClickHandler = (type: FormType) => {
+  const openAuthForm = (type: FormType) => {
     if (getActiveRoute() === Routes.game) {
       if (window.confirm(t('alertForExit'))) {
         changeRoute(Routes.home)
@@ -24,11 +29,35 @@ const OpenAuthForm = () => {
     }
   }
 
+  const signOut = async () => {
+    setIsAuthFormOpen(FormType.closed)
+
+    if (getActiveRoute() === Routes.game) {
+      if (window.confirm(t('alertForExit'))) {
+        await auth.signOut()
+        changeRoute(Routes.home)
+        return
+      }
+    }
+
+    await auth.signOut()
+  }
+
+  if (currentUser) {
+    return (
+      <div className={cx(styles.welcome, styles.container)}>
+        <p>
+          {t('welcome')}, {currentUser?.user?.displayName} ｜ <span onClick={signOut}>{t('signout')}</span>
+        </p>
+      </div>
+    )
+  }
+
   return (
-    <div className={styles.toggleForm}>
-      <p onClick={() => onClickHandler(FormType.signin)}>{t('signin')}</p>
+    <div className={cx(styles.toggleForm, styles.container)}>
+      <p onClick={() => openAuthForm(FormType.signin)}>{t('signin')}</p>
       <span>{t('or')}</span>
-      <p onClick={() => onClickHandler(FormType.signup)}>{t('signup')}</p>
+      <p onClick={() => openAuthForm(FormType.signup)}>{t('signup')}</p>
 
       <Modal isOpen={!!isAuthFormOpen} setIsOpen={() => setIsAuthFormOpen(false)} closeOnClickOutside={false}>
         <AuthForm type={isAuthFormOpen} />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,16 @@
 import OpenAuthForm from 'components/Form/AuthForm/OpenAuthForm'
-import { useAuthContext } from 'context/Auth/AuthContext'
 import { useInternalizationCtx } from 'context/Internalization/InternalizationContext'
-import { Routes, useRouterContext } from 'context/Router/RouterContext'
-import { auth } from 'hooks/useFirebase'
 
 import styles from './Header.module.scss'
 import Logo from './Logo'
 
 const Header = () => {
   const { t } = useInternalizationCtx()
-  const { currentUser } = useAuthContext()
-  const { changeRoute, getActiveRoute } = useRouterContext()
 
-  const signOut = async () => {
-    if (getActiveRoute() === Routes.game) {
-      if (window.confirm(t('alertForExit'))) {
-        await auth.signOut()
-        changeRoute(Routes.home)
-        return
-      }
-    }
-
-    await auth.signOut()
-  }
   return (
     <header className={styles.container}>
       <Logo />
-      {currentUser ? (
-        <p className={styles.welcome}>
-          {t('welcome')}, {currentUser?.user?.displayName} ｜ <span onClick={signOut}>{t('signout')}</span>
-        </p>
-      ) : (
-        <OpenAuthForm />
-      )}
+      <OpenAuthForm />
     </header>
   )
 }
